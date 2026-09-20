bbcac839f0e112274808903767d2a4ac3b359fc4
Benjamin Harbin <bharbin2023@fau.edu>
2026-09-20 18:51:47 -0400

    finished question 3 part c

--- Homework2/p3_HarbinBenjamin.py
+++ Homework2/p3_HarbinBenjamin.py
@@ -11,12 +11,36 @@ def add_friend(sn, user1, user2):
     else:
         sn[user1][1].append(user2)
         sn[user2][1].append(user1)
+def get_friends(sn, user1, distance):
+    try:
+        network = []
+        level = 0 #keeps track of friend level
+        friendList = sn[user1][1]
 
-sn = {}
-for i in range(2):
+        while level < distance:
+            nextList = []
+            for friend in friendList:
+                if friend != user1 and friend not in network:#skips adding the user into the
+                    network.append(friend)
+                for f in sn[friend][1]:#sees if the next friends have already been checked
+                    if f != user1 and f not in network and f not in nextList:
+                        nextList.append(f)
+                friendList = nextList
+            level +=1
+        return network
+    except KeyError:
+        print("User does not exist")
+
+sn = {'alice': ('Alice Smith', ['maria']),
+      'maria': ('Maria Cortez', ['alice', 'joe', 'david']),
+      'joe': ('Joseph Adams', ['maria', 'eve']),
+      'eve': ('Evelyn Cooper', ['joe']),
+      'david': ('David Benson', ['maria'])}
+
+'''for i in range(2):
     name = input("Enter your name: ")
     username = input("Enter your username: ")
     add_user(sn, username, name)
-print(sn)
-add_friend(sn, "alice", "maria")
-print(sn)
+print(sn)'''
+
+print(get_friends(sn,"alice", 1))
