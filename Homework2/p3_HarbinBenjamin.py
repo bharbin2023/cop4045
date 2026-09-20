bc639c8711e7f26abc8473347dc7a695b026fa39
Benjamin Harbin <bharbin2023@fau.edu>
2026-09-20 14:36:02 -0400

    finished problem 3 part b

--- Homework2/p3_HarbinBenjamin.py
+++ Homework2/p3_HarbinBenjamin.py
@@ -1,10 +1,22 @@
 def add_user(sn, username, fullname):
+    friends = []
     if username in sn:
         return False
     else:
-        sn[username] = (fullname, [])
+        sn[username] = (fullname, friends)
         return True
+def add_friend(sn, user1, user2):
+    if (user1 not in sn) or (user2 not in sn):
+        return False
+    else:
+        sn[user1][1].append(user2)
+        sn[user2][1].append(user1)
 
-socialNetwork = {}
-name = input("Enter your name: ")
-username = input("Enter your username: ")
+sn = {}
+for i in range(2):
+    name = input("Enter your name: ")
+    username = input("Enter your username: ")
+    add_user(sn, username, name)
+print(sn)
+add_friend(sn, "alice", "maria")
+print(sn)
